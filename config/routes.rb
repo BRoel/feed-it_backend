913d84118c1e9e269af1df217ea5206d12452d22
Brennan Roel <brennanroel@gmail.com>
2021-02-23 19:02:17 -0600

    added routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  namespace :api do
+    namespace :v1 do
+      resources :posts, only: [:index, :create, :destroy]
+      resources :comments, only: [:index, :create, :destroy]
+    end
+  end
+
+
 end
