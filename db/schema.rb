71bf42269f4106e78f320d662b5d6fb7277e8914
Brennan Roel <brennanroel@gmail.com>
2021-02-23 18:37:07 -0600

    Implemented migration for comments

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_24_002750) do
+ActiveRecord::Schema.define(version: 2021_02_24_003202) do
+
+  create_table "comments", force: :cascade do |t|
+    t.string "content"
+    t.integer "post_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["post_id"], name: "index_comments_on_post_id"
+  end
 
   create_table "posts", force: :cascade do |t|
     t.string "title"
@@ -19,4 +27,5 @@ ActiveRecord::Schema.define(version: 2021_02_24_002750) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  add_foreign_key "comments", "posts"
 end
