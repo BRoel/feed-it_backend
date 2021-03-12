8e3a8ca9bbd060a9fa72bf2f074ee3f8a78874d4
Brennan Roel <brennanroel@gmail.com>
2021-03-12 15:49:39 -0600

    added User resources and relationships

---
+++ app/serializers/user_serializer.rb
@@ -0,0 +1,6 @@
+class UserSerializer < ActiveModel::Serializer
+  attributes :id, :name, :username, :email
+
+  has_many :posts
+  has_many :comments
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   
+  resources :users
   namespace :api do
     namespace :v1 do
       resources :posts do
