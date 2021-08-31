75b62deb7a22aedcf384037d5fbddc92492a4960
Brennan Roel <brennanroel@gmail.com>
2021-08-31 17:40:13 -0500

    moved registration and sessions controllers, adjusted routes and adjusted cors

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,1 +1,5 @@
-Rails.application.config.session_store :cookie_store, key: '_salute_app', domain: 'http://localhost:3000'
+if Rails.env == 'production'
+    Rails.application.config.session_store :cookie_store, key: '_salute_app', domain: 'http://localhost:3000'
+else
+    Rails.application.config.session_store :cookie_store, key: '_salute_app'
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,13 @@
 Rails.application.routes.draw do
-  resources :sessions, only: [:create]
-  resources :registrations, only: [:create]
-  delete :logout, to: 'sessions#logout'
-  get :logged_in, to: 'sessions#logged_in'
+
   
   namespace :api do
     namespace :v1 do
+      resources :sessions, only: [:create]
+      resources :registrations, only: [:create]
+      delete :logout, to: 'sessions#logout'
+      get :logged_in, to: 'sessions#logged_in'
+      
       resources :posts do
         resources :comments
       end
