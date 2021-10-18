437fb48fb0a42ce6146df4915f73c2575991c4b9
Brennan Roel <brennanroel@gmail.com>
2021-10-18 18:04:06 -0500

    changed auth0 url for JWT

--- app/lib/json_web_token.rb
+++ app/lib/json_web_token.rb
@@ -7,7 +7,7 @@ class JsonWebToken
     JWT.decode(token, nil,
                true, # Verify the signature of this token
                algorithms: 'RS256',
-               iss: 'https://dev-brennan.us.auth0.com/',
+               iss: 'https://salute-vets.us.auth0.com/',
                verify_iss: true,
                aud: Rails.application.secrets.auth0_api_audience,
                verify_aud: true) do |header|
@@ -16,7 +16,7 @@ class JsonWebToken
   end
 
   def self.jwks_hash
-    jwks_raw = Net::HTTP.get URI("https://dev-brennan.us.auth0.com/.well-known/jwks.json")
+    jwks_raw = Net::HTTP.get URI("https://salute-vets.us.auth0.com/.well-known/jwks.json")
     jwks_keys = Array(JSON.parse(jwks_raw)['keys'])
     Hash[
       jwks_keys
