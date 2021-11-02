5615a347c57b47e9140b079d2cacd97751cd1d24
Brennan Roel <brennanroel@gmail.com>
2021-11-02 17:15:23 -0500

    edited direct uploads controller

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :posts do
+        post 'rails/active_storage/direct_uploads', to: 'direct_uploads#create'
         resources :comments
       end
     end
